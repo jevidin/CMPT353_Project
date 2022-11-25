b7be399d70e82285bf80e1161624eb93a84e6754
sean <sean3285@gmail.com>
2022-11-25 15:21:20 -0800

    add butterworth filter and run for all csv files at once

--- project.py
+++ project.py
@@ -4,18 +4,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
 from statsmodels.nonparametric.smoothers_lowess import lowess
+import glob
+from scipy import signal
 # from pykalman import KalmanFilter
 
-walk_data = sys.argv[1]
-
-cpu_data = pd.read_csv(walk_data, sep=',', parse_dates=['time'], names=['time', 'ax', 'ay', 'az', 'aT'], skiprows=1)
-cpu_data['time'] = pd.to_datetime(cpu_data.time.astype(str), format='%H:%M:%S:%f')
-print(cpu_data)
-cpu_data.info()
-plt.title("Walk")
-plt.xlabel("Time")
-plt.ylabel("Total acceleration")
-# plt.locator_params(axis='y', nbins=6)
-# plt.locator_params(axis='x', nbins=10)
-plt.scatter(cpu_data["time"], cpu_data["aT"])
-plt.show()
+for f in glob.glob(sys.argv[1]):
+    walk_data = pd.read_csv(f, sep=',', parse_dates=['time'], names=['time', 'ax', 'ay', 'az', 'aT'], skiprows=1)
+    walk_data['time'] = pd.to_datetime(walk_data.time.astype(str), format='%H:%M:%S:%f')
+    b, a = signal.butter(3, 0.08, btype='lowpass', analog=False)
+    lowpass = signal.filtfilt(b, a, walk_data["aT"])
+    plt.title("Walk")
+    plt.xlabel("Time")
+    plt.ylabel("Total acceleration")
+    plt.figure(figsize=(12,4))
+    plt.plot(walk_data["time"], walk_data["aT"], 'b.')
+    plt.plot(walk_data["time"], lowpass, 'r-')
+    plt.savefig(f'output/{f[4:-4]}.png')
