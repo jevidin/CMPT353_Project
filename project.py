033f0c1a08ac6d799cda139465bc6a4077b3d472
sean <sean3285@gmail.com>
2022-12-05 10:32:14 -0800

    find peaks

--- project.py
+++ project.py
@@ -9,16 +9,17 @@ from scipy import signal
 from scipy.fft import rfft, rfftfreq
 import datetime
 # from pykalman import KalmanFilter
-col = 'ay'
+col = 'aT'
 for f in glob.glob(sys.argv[1]):
     walk_data = pd.read_csv(f, sep=',', parse_dates=['time'], names=['time', 'ax', 'ay', 'az', 'aT'], skiprows=1)
     walk_data['time'] = pd.to_datetime(walk_data.time.astype(str), format='%H:%M:%S:%f')
     last_time = walk_data.iloc[-1]['time'] - datetime.timedelta(seconds=3)
-    print(last_time)
     walk_data = walk_data[walk_data['time'] <= last_time]
 
-    b, a = signal.butter(3, 0.05, btype='lowpass', analog=False)
+    b, a = signal.butter(3, 0.03, btype='lowpass', analog=False)
     lowpass = signal.filtfilt(b, a, walk_data[col])
+
+    # fourier transform
     yf = rfft(lowpass)
     xf = rfftfreq(len(walk_data.index), 1/205)
 
@@ -26,10 +27,18 @@ for f in glob.glob(sys.argv[1]):
     plt.plot(xf, np.abs(yf))
     plt.savefig(f'output/{f[4:-4]}fft.png')
 
+
+
     plt.title("Walk")
     plt.xlabel("Time")
     plt.ylabel("Total acceleration")
     plt.figure(figsize=(12,4))
+
+
     plt.plot(walk_data["time"], walk_data[col], 'b.')
     plt.plot(walk_data["time"], lowpass, 'r-')
+
+    peaks, _ = signal.find_peaks(lowpass, height=2)
+    plt.plot(walk_data['time'][peaks], lowpass[peaks], "x", color='magenta', markersize=8)
+    plt.plot(walk_data['time'], np.zeros_like(lowpass)+2, "--", color="gray")
     plt.savefig(f'output/{f[4:-4]}.png')
