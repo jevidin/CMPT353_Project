9b3fa79142a7d549252f54e6f301e5f36fda6e04
sean <sean3285@gmail.com>
2022-12-05 20:42:53 -0800

    polyfit datetime bug

--- project.py
+++ project.py
@@ -7,7 +7,11 @@ from statsmodels.nonparametric.smoothers_lowess import lowess
 import glob
 from scipy import signal
 from scipy.fft import rfft, rfftfreq
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.pipeline import make_pipeline
 import datetime
+import matplotlib.dates as mdates
 # from pykalman import KalmanFilter
 col = 'aT'
 for f in glob.glob(sys.argv[1]):
@@ -18,10 +22,25 @@ for f in glob.glob(sys.argv[1]):
 
     b, a = signal.butter(3, 0.02, btype='lowpass', analog=False)
     lowpass = signal.filtfilt(b, a, walk_data[col])
+    peak_heights = 1.5
+    peaks, _ = signal.find_peaks(lowpass, height=peak_heights)
+
+    # X = walk_data['time']
+    # y = lowpass
+    # model = make_pipeline(
+    #     PolynomialFeatures(degree=10, include_bias=True),
+    #     LinearRegression(fit_intercept=False)
+
+    # )
+    # model.fit(X, y)
+    # timearr = np.array(walk_data.time.values)
+    # print(lowpass.dtype)
+    # # timearr = mdates.date2num(walk_data.time)
+    # poly = np.poly1d(np.polyfit(timearr, lowpass, 10))
 
     # fourier transform
     yf = rfft(lowpass)
-    xf = rfftfreq(len(walk_data.index), 1/205)
+    xf = rfftfreq(len(walk_data.index), 205)
 
     plt.figure(figsize=(12,4))
     plt.plot(xf, np.abs(yf))
@@ -34,12 +53,11 @@ for f in glob.glob(sys.argv[1]):
     plt.ylabel("Total acceleration")
     plt.figure(figsize=(12,4))
 
+    plt.plot(walk_data['time'], poly[walk_data['time']])
 
-    plt.plot(walk_data["time"], walk_data[col], 'b.')
+    plt.plot(walk_data["time"], walk_data[col], 'b.', markersize=1)
     plt.plot(walk_data["time"], lowpass, 'r-')
 
-    peaks, _ = signal.find_peaks(lowpass, height=1.5)
-    print(peaks)
     plt.plot(walk_data['time'][peaks], lowpass[peaks], "x", color='magenta', markersize=8)
-    plt.plot(walk_data['time'], np.zeros_like(lowpass)+2, "--", color="gray")
+    plt.plot(walk_data['time'], np.zeros_like(lowpass)+peak_heights, "--", color="gray")
     plt.savefig(f'output/{f[4:-4]}.png')
